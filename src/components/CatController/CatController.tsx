4c0511b95c4fdccd1c38a71ba932c88de758bb5d
bora <nhatbaoxxd@gmail.com>
2024-03-21 08:56:10 +0700

    Multi States Selection and why shallow

--- src/components/CatController/CatController.tsx
+++ src/components/CatController/CatController.tsx
@@ -1,4 +1,5 @@
 import { useCatStore } from '@/stores/catStore'
+import { shallow } from 'zustand/shallow'
 
 export default function CatController() {
   // Cách gọi này sẽ làm CatController re-render không cần thiết
@@ -9,8 +10,22 @@ export default function CatController() {
   // const increaseSmallCats = useCatStore((state) => state.increaseSmallCats)
 
   // Cách gọi này sẽ không làm CatController re-render và ngắn gọn
-  const increaseBigCats = useCatStore.use.increaseBigCats()
-  const increaseSmallCats = useCatStore.use.increaseSmallCats()
+  // const increaseBigCats = useCatStore.use.increaseBigCats()
+  // const increaseSmallCats = useCatStore.use.increaseSmallCats()
+
+  // Cách gọi này sẽ không làm CatController re-render và ngắn gọn
+  // const { increaseBigCats, increaseSmallCats } = useCatStore(
+  //   (state) => ({
+  //     increaseBigCats: state.increaseBigCats,
+  //     increaseSmallCats: state.increaseSmallCats,
+  //   }),
+  //   shallow
+  // )
+
+  const [increaseBigCats, increaseSmallCats] = useCatStore(
+    (state) => [state.increaseBigCats, state.increaseSmallCats],
+    shallow
+  )
 
   return (
     <div className="box">
